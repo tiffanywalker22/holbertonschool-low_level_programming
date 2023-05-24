cfc752a64bfb43013e8041fda64b5fd4238711da
tiffanywalker22 <tbcasper16@gmail.com>
2023-05-24 13:01:04 -0700

    message

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* more headers goes there */
+
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (
+	return (0);
+}
