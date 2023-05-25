beb82963b4bf54db178999b4bc8bd3fc0b1ffb88
tiffanywalker22 <tbcasper16@gmail.com>
2023-05-25 09:33:46 -0700

    fixed answer

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'z'; alphabet >= 'a'; alphabet--)
+	{
+		putchar(alphabet);
+	}
+
+	putchar('\n');
+	return (0);
+}
