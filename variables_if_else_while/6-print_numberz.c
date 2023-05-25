affd57651d7fe5d89258e8960194a40628886ef0
tiffanywalker22 <tbcasper16@gmail.com>
2023-05-25 09:10:52 -0700

    message

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+		if (num < 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
