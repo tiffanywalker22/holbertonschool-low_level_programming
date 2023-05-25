96c62266bea3b2c8cfcbb0b45fb281aecb3ee4d3
tiffanywalker22 <tbcasper16@gmail.com>
2023-05-25 10:47:10 -0700

    commit

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _putchar - program prints _putchar, followed by new line
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ *
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+	int _putchar(putchar);
+{
+		return (write(1, &c, 1));
+}
+	
+
+
+
