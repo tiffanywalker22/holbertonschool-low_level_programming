815089f758765f5670a80a86f5be9dc592719d3f
tiffanywalker22 <tbcasper16@gmail.com>
2023-05-25 12:27:15 -0700

    commit

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * _islower - function checks for lowercase character
+ *
+ * Return: Always 0 (Success)
+ */
+	int _islower(int c)
+	{
+		return (c >= 'a' && c <= 'z');
+	}
+{
+		putchar('\n');
+}
