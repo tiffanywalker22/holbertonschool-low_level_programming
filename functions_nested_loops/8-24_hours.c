cb61e5e40a7f37b7a125ce139f1edff04d327da3
tiffanywalker22 <tbcasper16@gmail.com>
2023-05-27 10:26:07 -0700

    commit answer

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+  * jack_bauer - Prints every minute of the day of Jack Bauer
+  *
+  * 
+  */
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a <= 2; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
+			{
+				for (c = 0; c <= 5; c++)
+				{
+					for (d = 0; d <= 9; d++)
+					{
+						putchar(a + '0');
+						putchar(b + '0');
+						putchar(58);
+						putchar(c + '0');
+						putchar(d + '0');
+						putchar('\n');
+					}
+				}
+			}
+		}
+	}
+}
