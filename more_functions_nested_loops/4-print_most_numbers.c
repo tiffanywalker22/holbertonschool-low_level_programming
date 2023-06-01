5c674eb19791e3acb9163de65d03a688ce58f3c5
tiffanywalker22 <tbcasper16@gmail.com>
2023-06-01 13:56:11 -0700

    commit answer

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * print_most_numbers - prints numbers 0 to 9, except 2
+ *
+ * Return: returns nothing
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if ((i == 2) || (i == 4))
+			;
+		else
+			_putchar(i + '0');
+	}
+	_putchar('\n');
+}
