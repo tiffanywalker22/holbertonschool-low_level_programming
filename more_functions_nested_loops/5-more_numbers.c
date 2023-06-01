00b1d8bc7b551e4a934c3d24ebf48fbbc062659b
tiffanywalker22 <tbcasper16@gmail.com>
2023-06-01 14:20:43 -0700

    commit answer

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * _more_numbers - prints 10 times the numbers, from 0 to 14
+ *
+ * Return: returns nothing
+ *
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j <= 9; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+				_putchar('0' + 1);
+			_putchar((i % 10) + '0');
+		}
+	_putchar('\n');
+	}
+
+}
