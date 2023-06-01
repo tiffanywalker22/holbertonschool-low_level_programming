097ca8e5b2cb95ba36bb1b2b7831a5a057083f1c
tiffanywalker22 <tbcasper16@gmail.com>
2023-06-01 12:12:29 -0700

    first commit

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+  * isupper - checks for uppercase character
+  *
+  * @c: input letter or number
+  *
+  * Return: 1 if the character is lowercase, 0 otherwise
+  *
+  **/
+
+int _isupper(int c)
+{
+	int 1 = c
+	int i;
+	int r = 0;
+
+	for (i = 65 ; i < 91; i++)
+	{
+		if (i == 1)
+		{
+			r = 1;
+		}
+	}
+	return (r);
+}
