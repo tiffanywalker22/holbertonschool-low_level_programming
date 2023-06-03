65dac62267c462c73d0d5ec845cb535401490e30
tiffanywalker22 <tbcasper16@gmail.com>
2023-06-02 18:21:48 -0700

    commit answer

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - prints the length of a string
+ *
+ * @s: the string
+ *
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len ++;
+		s ++;
+	}
+	return len;
+
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define _main_h_
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
 
 
 
