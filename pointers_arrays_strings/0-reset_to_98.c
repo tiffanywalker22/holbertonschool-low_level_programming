9ca323b513e01091c74919205717db85584a77fe
tiffanywalker22 <tbcasper16@gmail.com>
2023-06-02 17:48:23 -0700

    commit answer

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * reset_to_98 - updates pointer value to 98
+ *
+ * @n: pointer
+ *
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
