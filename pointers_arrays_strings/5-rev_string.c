447f4396698a4216cad122331e494b035788b4aa
tiffanywalker22 <tbcasper16@gmail.com>
2023-06-02 20:15:22 -0700

    commit answer

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * rev_string - reverses a string
+ *
+ * @str- the string
+ *
+ * Returns: nothing
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	char temp;
+
+	for (i = 0; i < _strlen(s) / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[_strlen(s) - i - 1];
+		s[_strlen(s) - i - 1] = temp;
+	}
+}
+
