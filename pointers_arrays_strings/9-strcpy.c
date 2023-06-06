7738571e20d747a580d240039755db1bbaa0457d
tiffanywalker22 <tbcasper16@gmail.com>
2023-06-05 17:53:56 -0700

    commit answer

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+* strcpy - copies the string
+*
+* @dest: final location
+*
+* @src: string copy
+*
+* Return: returns nothing
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
