ba7222cca1fefcbeb526352cb49e91c105cf3c6a
tiffanywalker22 <tbcasper16@gmail.com>
2023-06-07 16:47:35 -0700

    commit answer

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include "2-strlen.c"
+/**
+ * _strcat - combines two strings
+ *
+ * @src - source string
+ *
+ * @dest - destination string
+ *
+ * return - a pointer to the string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int dest_len = _strlen(dest);
+	int src_len = _strlen(src);
+
+	int i; 
+	
+	for (i = 0; i < src_len; i++) 
+	{
+		dest[dest_len + i] = src[i];
+	}
+
+	dest[dest_len + src_len] = '\0';
+
+	return dest;
+}
