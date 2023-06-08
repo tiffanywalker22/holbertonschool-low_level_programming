07879d6bd9eb1f98bac452b48d618450e88a84de
tiffanywalker22 <tbcasper16@gmail.com>
2023-06-07 18:35:39 -0700

    change answer

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -16,15 +16,11 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i]; i++);
+	while (*(s1 + i) == *(s2 + i) && *(s1 + i))
+		i++;
 
-	if (s1[i] == '\0' && s2[i] == '\0')
-	{
+	if (*(s2 + i))
+		return (*(s1 + i) - *(s2 + i));
+	else
 		return (0);
-	}
-	if (s1[i] < s2[i])
-	{
-		return (-15);
-	}
-	return (15);
 }
