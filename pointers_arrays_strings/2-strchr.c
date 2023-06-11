48ca4457ec63be5486bdc847c9c73ceba19fd503
tiffanywalker22 <tbcasper16@gmail.com>
2023-06-11 13:20:12 -0700

    fixed betty

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -13,12 +13,11 @@ char *_strchr(char *s, char c)
 	int i = 0;
 
 	while (s[i] != '\0' && s[i] != c)
-			i++;
-
-	if (s[i] == c)
-		return (&s[i]);
-				
-	else
+		i++;
 	
+	if (s[i] == c)
+
+		return (&s[i]);
+	else
 		return (NULL);
 }
