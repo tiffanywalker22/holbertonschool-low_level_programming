bafc6e1690fc66550d5db550fb76f93dfd231ee1
tiffanywalker22 <tbcasper16@gmail.com>
2023-06-11 15:36:20 -0700

    commit answer

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strpbrk - searches a string for bytes
+ * @s: the string
+ * @accept: target matches
+ * return: a pointer to the string
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0, j;
+	
+	while (s[i] != '\0')
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				s = &s[i];
+				return (s);
+			}
+		}
+		i++;
+	}
+	return (NULL);
+}
+
