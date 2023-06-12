574b42646f2f70986b2eea469e4bec58fe267d23
tiffanywalker22 <tbcasper16@gmail.com>
2023-06-12 11:39:47 -0700

    fixed answer

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -12,14 +12,10 @@ void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	for (i = 0; j < 8; j++)
+	for (i = 0; i < 8; i++)
 	{
-		if (j == 7)
-		{
+		for (j = 0; j < 8; j++)
 			_putchar(a[i][j]);
-			_putchar('\n');
-		}
-		else
-			_putchar(a[i][j]);
+		_putchar('\n');
 	}
 }
