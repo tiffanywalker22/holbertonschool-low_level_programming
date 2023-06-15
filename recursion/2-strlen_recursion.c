2a2b2867e462b43ac7cc00850943188543c775c4
tiffanywalker22 <tbcasper16@gmail.com>
2023-06-14 20:14:21 -0700

    betty fix

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -6,11 +6,11 @@
  *
  * @s: the string
  *
- * Retrun: the string length
+ * Return: the string length
  */
 int _strlen_recursion(char *s)
 {
-	if(*s == '\0')
+	if (*s == '\0')
 		return (0);
 
 	s++;
