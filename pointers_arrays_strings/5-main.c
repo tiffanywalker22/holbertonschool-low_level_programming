fcefcf3a3ee37076f646b518d3d3278d37b68521
tiffanywalker22 <tbcasper16@gmail.com>
2023-06-23 16:54:54 -0700

    commit answer

--- pointers_arrays_strings/5-main.c
+++ pointers_arrays_strings/5-main.c
@@ -2,17 +2,16 @@
 #include <stdio.h>
 
 /**
- *  * main - check the code
- *   *
- *    * Return: Always 0.
- *     */
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	    char *s = "hello, world";
-	        char *f = "world";
-		    char *t;
+    char s[10] = "My School";
 
-		        t = _strstr(s, f);
-			    printf("%s\n", t);
-			        return (0);
+    printf("%s\n", s);
+    rev_string(s);
+    printf("%s\n", s);
+    return (0);
 }
