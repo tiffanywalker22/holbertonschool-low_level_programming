6118dac4bffe4c58ac2452dc49c49bb958762637
tiffanywalker22 <tbcasper16@gmail.com>
2023-06-26 15:27:36 -0700

    commit answer

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - allocate memory and set all values to 0
+ * @nmemb: size
+ * @size: sizeof(datatype)
+ * Return: pointer to calloc'd string
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int i = 0, l = 0;
+	char *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	l = nmemb * size;
+	p = malloc(l);
+
+	if (p == NULL)
+		return (NULL);
+
+	while (i < l)
+	{
+		p[i] = 0;
+		i++;
+	}
+
+	return (p);
+}
