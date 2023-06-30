2015cc3b25faea0d5310238a5c9302359ac7188e
tiffanywalker22 <tbcasper16@gmail.com>
2023-06-29 22:02:57 -0700

    commit answer 5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ *  * free_dog - Frees a new dog
+ *   * @d: Dog to free
+ *    *
+ *     * Return: Always Void
+ *      */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
