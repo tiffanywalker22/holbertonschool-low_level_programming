dd3ac76d9d7f63d05193ad790c61ce4fd01ed291
tiffanywalker22 <tbcasper16@gmail.com>
2023-07-03 16:19:18 -0700

    betty fix

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -7,12 +7,6 @@
 #include <stdarg.h>
 #include <stddef.h>
 
-typedef struct form
-{
-	char type;
-	void (*f)(va_list);
-} types_t;
-
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
