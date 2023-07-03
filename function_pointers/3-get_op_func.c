1b05e110d8146917d6e87497d91b70a92f081a91
tiffanywalker22 <tbcasper16@gmail.com>
2023-07-02 20:13:06 -0700

    commit answer 3

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * *get_op_func - function to select correct operation function
+ * @s: operation given
+ * Return: pointer to correction operation function
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+	
+	i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (*(ops[i].op) == *s)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+#include "3-calc.h"
+/**
+ * op_add - add
+ * @a: int 1
+ * @b: int 2
+ *
+ * Return: the sum of as and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtract
+ * a: int 1
+ * b: int 2
+ *
+ * Return: the difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply
+ * @a: int 1
+ * @b: int 2
+ *
+ * Return: multiple of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide
+ * @a: int 1
+ * b: int 2
+ *
+ * Return: division of a and b 
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulus for remainder
+ * @a: int 1
+ * @b: int 2
+ *
+ * Return: mod of a and b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
