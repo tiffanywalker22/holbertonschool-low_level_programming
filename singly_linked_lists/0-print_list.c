72921b864f7d785b062b43ca72b5db5da0440db6
tiffanywalker22 <tbcasper16@gmail.com>
2023-07-13 11:12:27 -0700

    setting up files

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_list - prints all elements of a list
+ *
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
