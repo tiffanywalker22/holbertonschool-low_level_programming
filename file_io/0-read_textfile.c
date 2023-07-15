7bfa1288b9ca8bd136f0ed4c6edfc861f71de9d3
tiffanywalker22 <tbcasper16@gmail.com>
2023-07-14 18:12:00 -0700

    commit answer 0

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+
+/**
+ * read_textfile- Read text file.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: number of bytes read and printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, printed, wrote;
+	char *buff;
+
+	buff = malloc(letters);
+	if (buff == NULL)
+		return (0);
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDWR);
+	if (fd == -1)
+		return (0);
+	printed = read(fd, buff, letters);
+	if (printed == -1)
+		return (0);
+	wrote = write(STDOUT_FILENO, buff, printed);
+	if (wrote == -1)
+		return (0);
+	if (close(fd) == -1)
+		return (0);
+	free(buff);
+	return (printed);
+}
