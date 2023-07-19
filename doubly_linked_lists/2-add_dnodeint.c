086b48941251c9768c65a4910e587996a7e35f37
tiffanywalker22 <tbcasper16@gmail.com>
2023-07-19 15:21:06 -0700

    commit answer 2

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * *add_dnodeint - add node to front of doubly linked list
+ * @head: pointer to head
+ * @n: node data
+ *
+ * Return: Number of Nodes
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
+
+	if (*head != NULL)
+		(*head)->prev = new_node;
+
+	*head = new_node;
+
+	return (new_node);
+}
